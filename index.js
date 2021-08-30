cca8d1b7f0bec20a6182aee9d4abcc5ff058dab8
DHaligowski <dhaligowski@gmail.com>
2021-08-30 14:59:32 -0400

    Update socket timeout

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
-// import { WebSocketServer } from "ws";
 const { WebSocketServer } = require("ws");
-// const WebSocket = require("ws");
 const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
 
 let currentLocation = {};
+let driver = false;
+let driverSocket = {};
+let socketCounter = 0;
+const SOCKET_RESET_TIMER = 200;
+const SEND_PING = 3000;
+
 wss.on("connection", (ws) => {
-  console.log("websocket connection open");
   var id = setInterval(function () {
-    // ws.send(JSON.stringify(currentLocation), function () {});
-    ws.send("KeepSocketAlive", function () {});
-  }, 30000);
-  // console.log("connection open, sending location", currentLocation);
-  ws.send(JSON.stringify(currentLocation));
-  // console.log("clients", wss.clients);
+    ws.send(JSON.stringify(driver), function () {
+      if (ws === driverSocket) socketCounter++;
+
+      // console.log("socketCounter", socketCounter);
+
+      if (socketCounter === SOCKET_RESET_TIMER) {
+        ws.send(JSON.stringify(false));
+        socketCounter = 0;
+        driver = false;
+        driverSocket = {};
+        currentLocation = {};
+        ws.close();
+      }
+    });
+  }, SEND_PING);
+
+  if (driver) ws.send(JSON.stringify(currentLocation));
+
   ws.on("message", (message) => {
-    currentLocation = JSON.parse(message);
-    // console.log("message received:", currentLocation);
-    // console.log("Sending message back");
-    // ws.send(JSON.stringify(data));
-    wss.broadcast(JSON.parse(message));
-    // let msg=JSON.parse(message);
-    // wss.clients.forEach((client) => client.send(message));
+    //driver check, can only have 1 logged in
+    if (driver && ws !== driverSocket) ws.send(JSON.stringify(false));
+    else {
+      try {
+        currentLocation = JSON.parse(message);
+
+        if (!driver) {
+          driverSocket = ws;
+          driver = true;
+        }
+
+        wss.broadcast(JSON.parse(message));
+      } catch (error) {
+        console.log("an error occured...", error);
+      }
+    }
   });
   ws.on("close", function () {
-    console.log("websocket connection close");
+    // console.log("websocket connection close");
+    if (driverSocket === ws) {
+      // console.log("driver is logging out");
+      driver = false;
+      driverSocket = {};
+      currentLocation = {};
+    }
     clearInterval(id);
   });
-  // ws.send("something");
 });
 
 wss.broadcast = (msg) => {
-  // console.log(msg);
   wss.clients.forEach((client) => {
-    // if (client !== ws && client.readyState === WebSocket.OPEN)
     client.send(JSON.stringify(msg));
   });
 };
